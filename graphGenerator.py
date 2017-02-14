05110fa83f1604044a8465b82da22a2227b1ea9f
Catherine <catherinema828@gmail.com>
2017-02-13 21:34:05 -0800

    added balsamiq mockups and ER graph/code to ppt

--- graphGenerator.py
+++ graphGenerator.py
@@ -2,40 +2,25 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import random as rn 
 
-nodeNum = 100
+num_nodes = 20
 p = 0.15
-pathNum = 5 # Get a path for the consumer- company network
+path_len = 5
 
-er = nx.erdos_renyi_graph(nodeNum, p)
-# nx.clustering(er)
+er = nx.erdos_renyi_graph(num_nodes, p)
 nx.draw(er)
 
-'''
-Generating an Erdos-Renyi Graph:
-https://networkx.github.io/documentation/development/tutorial/tutorial.html
-https://networkx.github.io/documentation/development/reference/generators.html
-
-'''
-#plt.show()
-#plt.savefig("erdos-renyi.png")
-
-#print(er.number_of_nodes()) 
-#print(er.edges())  # get all edges
-#print(er.edges(0)) # get edges of node 0 
+plt.show()
+plt.savefig('erdos-renyi.png')
 
 path = []
-# random start point
-node = rn.randint(0,nodeNum)
+node = rn.randint(0,num_nodes)
 path.append(node)
-for i in range(pathNum):
-	edges = er.edges(node)
-	#print(edges)
-	itr = rn.randint(0, len(edges)-1)
-	nextNode = edges[itr][1]
-	path.append(nextNode)
-	node = nextNode
 
-print(path) 
-# get a path for the consumer to find. (give them a start node. 
-# They are to try to find the end node. We know it is possible
-#  because we have a path)
+for i in range(path_len):
+	edges = er.edges(node)
+	itr = rn.randint(0, len(edges)-1)
+	next_node = edges[itr][1]
+	path.append(next_node)
+	node = next_node
+
+print(path)
