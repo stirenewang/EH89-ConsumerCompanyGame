346430a361a214500cf84c4299f7cd0135d1b6d0
Catherine <catherinema828@gmail.com>
2017-03-04 22:48:40 -0800

    kwoooo

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-import flask
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def selection():
+	return render_template('selection.html')
+
+@app.route('/company')
+def company():
+	return render_template('company.html')
+
+@app.route('/consumer')
+def consumer():
+	return render_template('consumer.html')
+
+if __name__ == '__main__':
+	app.run()
